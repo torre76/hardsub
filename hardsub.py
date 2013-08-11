ec42aa636a07ddbb793262b93231f40fb3e808a4
Gian Luca Dalla Torre <gianluca.dallatorre@gmail.com>
2013-08-11 15:11:30 +0200

    Add header and raw argparse support

--- hardsub.py
+++ hardsub.py
@@ -5,12 +5,17 @@
 	hardsub.py: Encode video with hard subtitles
 """
 
+import argparse
+import platform
 import shutil
+import sys
+
+import colorama
 
 __author__ = "Gian Luca Dalla Torre"
 __copyright__ = "Copyright 2013, Gian Luca Dalla Torre"
 __credits__ = []
-__license__ = "GPL"
+__license__ = "GPL v.3"
 __version__ = "0.1"
 __maintainer__ = "Gian Luca Dalla Torre"
 __email__ = "gianluca.dallatorre@gmail.com"
@@ -19,6 +24,7 @@ __status__ = "Alpha"
 # List of required executables on a Linux Box used to accomplish
 # hard subbing
 REQUIRED_EXECUTABLES = (
+	'file',
 	'mencoder',
 	'mplayer',
 	'mkvextract',
@@ -26,6 +32,21 @@ REQUIRED_EXECUTABLES = (
 	'MP4Box'
 	
 )
+
+def header():
+	"""
+		Print Script Header
+	"""
+	print(colorama.Style.BRIGHT + "HardSub " + colorama.Style.NORMAL + "v." + colorama.Style.BRIGHT+ " {}".format(__version__) + colorama.Style.NORMAL)
+	print (__copyright__)
+	print ("Licensed under " + colorama.Style.BRIGHT+ "{}".format(__license__) + colorama.Style.NORMAL + " license.\n")
+
+def check_platform():
+	"""
+		Check if the platform is Linux
+		:returns: boolean - True if the platform is Linux, False otherwise
+	"""
+	return platform.system() == "Linux"
 
 def check_prerequisites():
 	"""
@@ -39,5 +60,33 @@ def check_prerequisites():
 			missing_executables.append(executable)
 	return (len(missing_executables) == 0, tuple(missing_executables))
 
+def build_argument_parser():
+	"""
+		Build an ArgumentParser instance suitable for this script
+		:returns: ArgumentParser -- ArgumentParser for this script
+	"""
+	ap = argparse.ArgumentParser(
+		prog="hardsub",
+		description="Encode video with hardsubs"
+	)
+	ap.add_argument("<source_dir>", help="Directory that contains video files to hardsub. This directory also had to hold srt files with the same name of video file")
+	ap.add_argument("-o", "--output", required=True, help="States the output directory for hardsubbed video.", metavar="<output_dir>")
+	return ap
+
 if __name__ == "__main__":
-	print (check_prerequisites())
+	colorama.init()
+	header()
+	# First check platform and, if it is not OK, programm will exit
+	if not check_platform():
+		print ("Sorry but, for now, only" + colorama.Style.BRIGHT + " Linux " + colorama.Style.NORMAL + "platform is supported.")
+		sys.exit(1)
+	# Second, check for all executables that are required by this script
+	executables_found, missing_executables = check_prerequisites()
+	if not executables_found:
+		print ( colorama.Style.BRIGHT + "Some dependencies for this script are missing. "  + colorama.Style.NORMAL + "Please check that the following packages are installed on this Linux Box:")
+		for me in missing_executables:
+			print ("\t- "  + colorama.Style.BRIGHT + "{}".format(me)  + colorama.Style.NORMAL);
+		sys.exit(2)
+	# Check for script parameters
+	ap = build_argument_parser()
+	ap.parse_args(sys.argv[1:])
