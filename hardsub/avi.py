d8900223643d2ee2616e292208a5f881488fd5d4
Gian Luca Dalla Torre <gianluca.dallatorre@gmail.com>
2013-10-15 00:02:51 +0200

    Add missing inverted commas on commands source file(s).

--- hardsub/avi.py
+++ hardsub/avi.py
@@ -118,7 +118,7 @@ def mux_audio_video(file_name, output_dir, verbose=False, debug=False):
 
 	# Gather data for the progress bar.
 	# If you have ffmpeg you have ffprobe, so it is not checked in REQUIRED_EXECUTABLES
-	command = '{ffprobe} -show_streams {video_input}'.format(
+	command = '{ffprobe} -show_streams "{video_input}"'.format(
 		ffprobe=which('ffprobe')[0],
                 video_input=video_file,
 	)
@@ -135,7 +135,7 @@ def mux_audio_video(file_name, output_dir, verbose=False, debug=False):
 			tot_frames = int(thread.match.group(1))
 	thread.close()	 
 
-	command = '{ffmpeg} -y {video_input} {input_params} -c copy -map 0:0 {map_params} "{dest_file}"'.format(
+	command = '{ffmpeg} -y "{video_input}" {input_params} -c copy -map 0:0 {map_params} "{dest_file}"'.format(
 		ffmpeg=which('ffmpeg')[0],
                 video_input=video_file,
 		input_params=' '.join(input_param),
